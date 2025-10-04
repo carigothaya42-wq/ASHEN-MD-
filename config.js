0da1504923398e8d8f9aa0aa7ccc511e09e904b0
carigothaya42-wq <carigothaya42@gmail.com>
2025-10-04 20:07:24 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? 'KAVIDU-MD=7xoX0B7K#-Z1Z_1gNfCrUinhtVYsW1syyqnQ4YLmHofUD5vvBIl8' : process.env.SESSION_ID, 
+PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
+SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
+
+};
+
